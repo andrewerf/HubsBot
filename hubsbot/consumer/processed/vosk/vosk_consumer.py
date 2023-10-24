a8609a16471c3735e7aeeac7085035d422fe16a8
Andrew <andrew-aralov@yandex.ru>
2023-10-24 13:09:04 +0300

    Make vosk_consumer.py work asynchronously.

--- hubsbot/consumer/processed/vosk/vosk_consumer.py
+++ hubsbot/consumer/processed/vosk/vosk_consumer.py
@@ -5,6 +5,7 @@ from av import AudioFrame
 from pydub import AudioSegment
 import io
 import itertools
+from aioprocessing import AioProcess, AioPipe
 
 from vosk import Model, KaldiRecognizer
 
@@ -17,14 +18,34 @@ def batched(x, n):
         yield batch
 
 
+def vosk_server(conn, model: Model, framerate: int):
+    """
+    This function is intended to be called in a separate thread (process actually) to allow non-blocking speech recognition.
+    """
+    rec = KaldiRecognizer(model, framerate)
+    rec.SetWords(True)
+    rec.SetPartialWords(True)
+
+    stop = False
+    while not stop:
+        msg = conn.recv()
+        if isinstance(msg, bool):
+            stop = msg
+        else:
+            rec.AcceptWaveform(msg)
+            ret = json.loads(rec.FinalResult())
+            conn.send(ret)
+
+
 class VoskVoiceConsumer(PhrasesVoiceConsumer):
     def __init__(self, track: MediaStreamTrack):
         super().__init__(track)
         self.model = Model(lang='ru')
         self.framerate = 48000
-        self.rec = KaldiRecognizer(self.model, self.framerate)
-        self.rec.SetWords(True)
-        self.rec.SetPartialWords(True)
+        conn1, conn2 = AioPipe(True)
+        self.vosk_process = AioProcess(target=vosk_server, args=(conn1, self.model, self.framerate))
+        self.vosk_process.start()
+        self.conn = conn2
 
     async def on_phrase_text(self, text: str):
         pass
@@ -37,11 +58,16 @@ class VoskVoiceConsumer(PhrasesVoiceConsumer):
             segment = segment + AudioSegment.from_raw(s, sample_width=frame.format.bytes,
                                                       channels=len(frame.layout.channels), frame_rate=frame.sample_rate)
 
-        silence = AudioSegment.silent(1000, self.framerate)
+        silence = AudioSegment.silent(2000, self.framerate)
         segment = silence + segment + silence
         segment = segment.set_frame_rate(self.framerate)
         segment = segment.set_channels(1)
-        self.rec.AcceptWaveform(segment.raw_data)
+        await self.conn.coro_send(segment.raw_data)
 
-        res = json.loads(self.rec.FinalResult())['text']
+        res = (await self.conn.coro_recv())['text']
         await self.on_phrase_text(res)
+
+    async def stop(self):
+        await self.conn.coro_send(False)
+        await self.vosk_process.coro_join()
+        await self.vosk_process.close()
